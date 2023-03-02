9021ef7722c0eb10daa267ba9d2181c73c9a58ff
EQuinteros11 <2508722019@mail.utec.edu.sv>
2023-03-02 17:39:03 -0600

    Avance
    primer

---
+++ Listado_Medicamentos_y_Farmacias_que_distribuyen/app/src/test/java/utec/edu/sv/listado_medicamentos_y_farmacias_que_distribuyen/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package utec.edu.sv.listado_medicamentos_y_farmacias_que_distribuyen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
