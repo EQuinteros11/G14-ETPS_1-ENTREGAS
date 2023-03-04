8547ce6c8faa6f0b92e13deb3a75da5e9e875a89
EQuinteros11 <2508722019@mail.utec.edu.sv>
2023-03-04 12:53:01 -0600

    Prueba login
    Prueba 1

---
+++ Listado_Medicamentos_y_Farmacias_que_distribuyen/app/src/main/java/utec/edu/sv/listado_medicamentos_y_farmacias_que_distribuyen/Home.java
@@ -0,0 +1,14 @@
+package utec.edu.sv.listado_medicamentos_y_farmacias_que_distribuyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- Listado_Medicamentos_y_Farmacias_que_distribuyen/app/src/main/java/utec/edu/sv/listado_medicamentos_y_farmacias_que_distribuyen/MainActivity.java
+++ Listado_Medicamentos_y_Farmacias_que_distribuyen/app/src/main/java/utec/edu/sv/listado_medicamentos_y_farmacias_que_distribuyen/MainActivity.java
@@ -2,13 +2,55 @@ package utec.edu.sv.listado_medicamentos_y_farmacias_que_distribuyen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+EditText edtUsuario, edtPass;
+Button btnIniciar;
+String nombre[];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtPass = findViewById(R.id.edtContra);
+        btnIniciar = findViewById(R.id.btnIniciar);
+
+       /* for(int i=1; i<5; i++) {
+
+                btnIniciar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        startActivity(new Intent(getApplicationContext(),Home.class));
+                        }
+                    }
+                });
+       }*/
+
+    }
+    public void Home(View v){
+        nombre = new String [4];
+        String nombre []= {"EliasQ","IvanM","Edward","Jessi"};
+        String use, pass;
+        use = edtUsuario.getText().toString();
+        pass = edtPass.getText().toString();
+        for(int i=0; i<5;i++){
+            System.out.println(nombre[0]);
+            if(use.equals(nombre[i]) && pass.equals("123")){
+                Intent mostrar = new Intent(this,Home.class);
+                startActivity(mostrar);
+                i=6;
+            }
+            else {
+
+            }
+       }
     }
 }
