f7c8396a97f0ce2367e1fcaefe06e2576976a11f
Iván Landaverde <2502882019@mail.utec.edu.sv>
2023-03-26 22:01:02 -0600

    Avanzando

---
+++ Listado_Medicamentos_y_Farmacias_que_distribuyen/app/src/main/java/utec/edu/sv/listado_medicamentos_y_farmacias_que_distribuyen/Datos_Generales_Perfil.java
@@ -0,0 +1,14 @@
+package utec.edu.sv.listado_medicamentos_y_farmacias_que_distribuyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Datos_Generales_Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_generales_perfil);
+    }
+}
