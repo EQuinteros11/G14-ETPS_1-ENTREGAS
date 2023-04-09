129b98e8fd153b3de80cfab57385915df3cd13c4
Iván Landaverde <2502882019@mail.utec.edu.sv>
2023-04-09 15:16:42 -0600

    Front avanzando

---
+++ Listado_Medicamentos_y_Farmacias_que_distribuyen/app/src/main/java/utec/edu/sv/listado_medicamentos_y_farmacias_que_distribuyen/Registro_Usuario.java
@@ -0,0 +1,16 @@
+package utec.edu.sv.listado_medicamentos_y_farmacias_que_distribuyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuario);
+
+
+    }
+}
