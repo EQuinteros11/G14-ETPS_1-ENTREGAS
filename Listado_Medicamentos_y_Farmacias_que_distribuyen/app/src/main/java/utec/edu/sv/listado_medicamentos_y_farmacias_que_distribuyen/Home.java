21899a188ad6c0a8eb7072f1a13dfa20cfed5a3a
Iván Landaverde <2502882019@mail.utec.edu.sv>
2023-05-28 20:02:55 -0600

    Menu lateral comenzado

--- Listado_Medicamentos_y_Farmacias_que_distribuyen/app/src/main/java/utec/edu/sv/listado_medicamentos_y_farmacias_que_distribuyen/Home.java
+++ Listado_Medicamentos_y_Farmacias_que_distribuyen/app/src/main/java/utec/edu/sv/listado_medicamentos_y_farmacias_que_distribuyen/Home.java
@@ -1,6 +1,9 @@
 package utec.edu.sv.listado_medicamentos_y_farmacias_que_distribuyen;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +13,17 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class Home extends AppCompatActivity {
     TextView tvverusu;
     ListView lstFarmacias;
     Button btnListaFarmacias, btCategoria1;
     ArrayAdapter<CharSequence> adapter;
     ArrayAdapter adaptador;
+    DrawerLayout drawerLayout;
+    Toolbar tlbar;
+    NavigationView nView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,14 @@ public class Home extends AppCompatActivity {
         lstFarmacias = findViewById( R.id.listFarmaciasMasVisitadas );
         btnListaFarmacias = findViewById( R.id.btnVerFarmacias );
         btCategoria1 = findViewById( R.id.btnCategoria1 );
+        drawerLayout = findViewById( R.id.drawLayout );
+        tlbar = findViewById( R.id.toolbar );
+        nView = findViewById( R.id.navView );
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawerLayout, tlbar, R.string.open, R.string.close );
+        drawerLayout.addDrawerListener( toggle );
+        toggle.syncState();
+        toggle.getDrawerArrowDrawable().setColor( getResources().getColor( R.color.white, null ) );
 
         Bundle bundle = getIntent().getExtras();
         tvverusu =findViewById(R.id.tvVerUsuario);
